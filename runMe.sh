9aa0a503033fe41dfb233381aa948638e5bd3235
Alonso Isidoro Roman <alonsoir@gmail.com>
2018-12-13 13:45:56 +0100

    updating runMe.sh to show frequencies.

--- runMe.sh
+++ runMe.sh
@@ -3,12 +3,22 @@ start_time=$(date +%s)
 actual_date=`date`
 clear
 rm *txt
-echo "Running with arguments $* at $actual_date"
+echo "Running with $# arguments. $* at $actual_date" >> "output.txt"
+if [ "$#" != 4 ]
+then
+	echo "Insuficients arguments."
+	echo "sh runMe.sh em 10 /PATH_TO_Euromillones2004_2018.csv /PATH_TO_Primitiva.csv"
+	exit;
+fi;
 # $1 em,primitiva,all,all-experimental
 # $2 numIterations
 # $3 PATH_TO_Euromillones2004_2018.csv
 # $4 PATH_TO_Primitiva.csv
-clear && java -cp target/MyStreamjava8-1.0.jar com.aironman.ApiStreamTests "$1" "$2" "$3" "$4" >> "output_$actual_date.txt"
+type=$1
+numIterations=$2
+PATH_TO_Euromillones2004_2018=$3
+PATH_TO_Primitiva=$4
+clear && java -cp target/MyStreamjava8-1.0.jar com.aironman.ApiStreamTests "$type" "$numIterations" "$PATH_TO_Euromillones2004_2018" "$PATH_TO_Primitiva" >> "output.txt"
 
 # Me quedo con los ganadores y las guardo en un fichero
 while read linea;
@@ -28,11 +38,12 @@ do
 	if [[ "${linea}" =~ "Winner5" ]] 
 	then echo $linea;
 	fi;
-done < "output_$actual_date.txt" > "final_winners_$actual_date.txt"
+done < "output.txt" > "final_winners.txt"
 
 # echo "pruning winners data "
 # echo "tr, sort, uniq, awk, print..."
-(tr ' ' '\n' | sort | uniq -c | awk '{print "winner " $2 " appeared "$1 " times."}') < "final_winners_$actual_date.txt" > "final_output_winners_$actual_date.txt"
+echo "numIterations $numIterations"
+(tr ' ' '\n' | sort | uniq -c | awk '{print "winner " $2 " appeared "$1 " times of '"$numIterations"'. Frequency is "$1*100 / '"$numIterations"'"%"}') < "final_winners.txt" > "final_output_winners.txt"
 
 # Me quedo con las estrellas y las guardo en un fichero
 while read linea;
@@ -43,23 +54,23 @@ do
 	if [[ "${linea}" =~ "Star2" ]] 
 	then echo $linea;
 	fi;
-done < "output_$actual_date.txt" > "final_star_$actual_date.txt"
+done < "output.txt" > "final_star.txt"
 
 # echo "pruning stars data "
 # echo "tr, sort, uniq, awk, print..."
-(tr ' ' '\n' | sort | uniq -c | awk '{print "star " $2 " appeared "$1 " times."}') < "final_star_$actual_date.txt" > "final_output_star_$actual_date.txt"
+(tr ' ' '\n' | sort | uniq -c | awk '{print "star " $2 " appeared "$1 " times of '"$numIterations"'. Frequency is "$1*100 / '"$numIterations"'"%"}') < "final_star.txt" > "final_output_star.txt"
 
 # borrando temporales
 # echo "Deleting temporal files."
-rm "final_star_$actual_date.txt"
-rm "final_winners_$actual_date.txt"
-mv "output_$actual_date.txt" output.txt
+# rm "final_star.txt"
+# rm "final_winners.txt"
+# mv "output_$actual_date.txt" output.txt
 finish_time=$(date +%s)
 # echo "Showing general results."
 # cat output.txt
 echo "A general file named output.txt has been generated. "
 echo "Showing winners."
-cat "final_output_winners_$actual_date.txt"
+cat "final_output_winners.txt"
 echo "Showing stars."
-cat "final_output_star_$actual_date.txt"
+cat "final_output_star.txt"
 echo "Done. Time duration: $((finish_time - start_time)) secs."
